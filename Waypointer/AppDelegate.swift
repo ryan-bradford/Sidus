2b40310bbc0b69c9cb27f12b6ed9bd22bb52d7f1
ryanb <ryanbradford333@gmail.com>
2016-07-13 22:36:03 -0400

    Updated the logo.

--- Waypointer/AppDelegate.swift
+++ Waypointer/AppDelegate.swift
@@ -27,8 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     
     func applicationDidEnterBackground(application: UIApplication) {
         classes.isInForeground = false
-        let view = application.windows[0].rootViewController as! ViewController
-        //view.removeStuff()
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
